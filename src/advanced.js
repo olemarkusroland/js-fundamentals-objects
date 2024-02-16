7951bda323ae268372a2f3d0d25680e43f783024
Ole Markus Roland <ole.markus.roland@no.experis.com>
2024-02-16 15:44:52 +0100

    Complete

--- src/advanced.js
+++ src/advanced.js
@@ -1,13 +1,14 @@
 // 1. create a const variable called item1 and set it to an object with the following properties:
 // name of 'item1', price of 400, and quantity of 4, hasDiscount of true, discount of 0.3
-const item1 = undefined
+const item1 = {name: 'item1', price: 400, quantity: 4, hasDiscount: true, discount: 0.3}
 
 // 2. create a const variable called item2 and set it to an object with the following properties:
 // name of 'item2', price of 300, and quantity of 3, hasDiscount of false, discount of 0.0
-const item2 = undefined
+const item2 = {name: 'item2', price: 300, quantity: 3, hasDiscount: false, discount: 0.0 }
+
 // 3. create a const variable called shoppingBasket and set it to an object with the following properties:
 // items - an array containing item1 and item2, and shopName called 'Sheepmart'
-const shoppingBasket = undefined
+const shoppingBasket = {items: [item1, item2], shopName: 'Sheepmart'}
 
 module.exports = {
   item1,
--- src/extension.js
+++ src/extension.js
@@ -19,10 +19,21 @@ const book = {
 const isbn13 = '978-0132350884'
 
 // 1. Set this to the book name - using the book object
-const name = ''
+const name = book.name
 
 // 2. Set this to the isbn 10 value - using the book object
-const isbn10 = ''
+const isbn10 = book.isbn.isbn10
+
+// Other necessary changes, based on test failure messages.
+book.category = 'Programming'
+
+book.pages = 464
+
+delete book.isbn.asin
+
+delete book.dimensions
+
+book.isbn.isbn13 = isbn13
 
 // Do not modify this basket object directly
 const basket = {
@@ -42,10 +53,16 @@ const basket = {
 }
 
 // 3. Set this variable to the length of the baskets voucher codes array - using the basket object
-const numberOfVoucherCodes = null
+const numberOfVoucherCodes = basket.voucherCodes.length
 
 // 4. Set this variable to the first element in of the baskets voucher codes array - using the basket object
-const firstVoucherCode = null
+const firstVoucherCode = basket.voucherCodes[0]
+
+// Other necessary changes, based on test failure messages.
+const orangeItem = { name: 'Oranges', price: 0.75, quantity: 4 }
+basket.items.push(orangeItem)
+
+basket.items.find(item => item.name === "Apple").price = 2
 
 // Do not edit this exported object
 module.exports = {
